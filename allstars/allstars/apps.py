f8e26a8d14ef545e812b20292e46fd59b471150b
Kyle <kylegomi@gmail.com>
2018-12-07 23:58:54 -0500

    got django to launch

---
+++ allstars/allstars/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AllstarsConfig(AppConfig):
+    name = 'allstars'
---
+++ allstars/venv_as/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/beefbeverly/Documents/GitHub/SI664-Final-Project-Mens-Basketball/allstars/venv_as/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
