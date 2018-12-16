6b56a5268276f366fef159443b2916ebfeddc047
Kyle <kylegomi@gmail.com>
2018-12-16 18:33:17 -0500

    test python import secret

--- allstars/mysite/settings.py
+++ allstars/mysite/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+from .secret import Secret
 import os
-from settings_secret import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
